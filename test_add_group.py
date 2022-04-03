5e86e32f96f17e9b52a8ea867782ff5467d94464
Den21rus <denis.yuryevich.p@gmail.com>
2022-04-03 21:54:08 +0300

    доработка

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -7,20 +7,21 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
     
-    def test_add_group(self):
+    def test_test_add_group(self):
+        success = True
         wd = self.wd
         wd.get("https://github.com/login?return_to=https%3A%2F%2Fgithub.com%2Fsignup%3Fref_cta%3DSign%2Bup%26ref_loc%3Dheader%2Blogged%2Bout%26ref_page%3D%252F%253Cuser-name%253E%252F%253Crepo-name%253E%26source%3Dheader-repo%26source_repo%3DDen21rus%252Fbarancev_training")
-        wd.find_element_by_id("login_field").click()
-        wd.find_element_by_id("login_field").clear()
-        wd.find_element_by_id("login_field").send_keys("Den21rus")
-        wd.find_element_by_id("password").click()
-        wd.find_element_by_id("password").clear()
-        wd.find_element_by_id("password").send_keys("Htc777@pux")
+        wd.find_element_by_name("login_field").click()
+        wd.find_element_by_name("login_field").clear()
+        wd.find_element_by_name("login_field").send_keys("Den21rus")
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys("Htc777@pux")
         wd.find_element_by_xpath("//div[@id='login']/div[4]").click()
         wd.find_element_by_name("commit").click()
         wd.get("https://github.com/")
