0df70d4baddb5b09351149b84cd179ad672cdb67
Den21rus <denis.yuryevich.p@gmail.com>
2022-04-10 18:26:37 +0300

    рефактор, выделены вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -1,44 +1,83 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
-class test_add_group(unittest.TestCase):
+
+class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-    
-    def test_test_add_group(self):
-        success = True
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_untitled_test_case(self):
         wd = self.wd
-        wd.get("https://github.com/login?return_to=https%3A%2F%2Fgithub.com%2Fsignup%3Fref_cta%3DSign%2Bup%26ref_loc%3Dheader%2Blogged%2Bout%26ref_page%3D%252F%253Cuser-name%253E%252F%253Crepo-name%253E%26source%3Dheader-repo%26source_repo%3DDen21rus%252Fbarancev_training")
-        wd.find_element_by_name("login_field").click()
-        wd.find_element_by_name("login_field").clear()
-        wd.find_element_by_name("login_field").send_keys("Den21rus")
-        wd.find_element_by_name("password").click()
-        wd.find_element_by_name("password").clear()
-        wd.find_element_by_name("password").send_keys("Htc777@pux")
-        wd.find_element_by_xpath("//div[@id='login']/div[4]").click()
-        wd.find_element_by_name("commit").click()
+        self.open_home_page(wd)
+        self.login(wd, Group(username="Den21rus", password="Htc777@pux"))
+        self.open_project_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='New organization'])[1]/following::summary[1]").click()
+        wd.find_element_by_xpath(
+            "//button[@class='dropdown-item dropdown-signout']").click()
+
+    def open_project_page(self, wd):
+        # Открываем окно с проектами
         wd.get("https://github.com/")
-        wd.find_element_by_xpath("//div[@id='repos-container']/ul/li/div/div/a/span").click()
-    
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='New project'])[1]/following::summary[1]").click()
+        wd.find_element_by_link_text("Your projects").click()
+        wd.find_element_by_xpath("//main[@id='js-pjax-container']/div/div/div/div[2]/div/nav/a[2]").click()
+        wd.find_element_by_link_text("barancev_training").click()
+
+    def login(self, wd, group):
+        wd.find_element_by_id("login_field").click()
+        wd.find_element_by_id("login_field").clear()
+        wd.find_element_by_id("login_field").send_keys(group.username)
+        wd.find_element_by_id("password").click()
+        wd.find_element_by_id("password").clear()
+        wd.find_element_by_id("password").send_keys(group.password)
+        wd.find_element_by_name("commit").click()
+
+    def open_home_page(self, wd):
+        wd.get("https://github.com/login")
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.wd.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
         self.wd.quit()
+        self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
